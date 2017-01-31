e6b386ed3d2ba359d75bea5cef3568bb5d8de652
Николай Акопов <asoman97@gmail.com>
2017-01-31 21:41:01 +0300

    Начал писать скрипты колеса, цели и основной игровой скрипт

---
+++ Solys/Assets/Scripts/Target/TargetLogic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLogic : MonoBehaviour {
+
+	public delegate void TargetAction();
+	public static TargetAction TargetReached;
+
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player" && TargetReached!=null)
+		{
+			TargetReached();
+		}
+	}
+}
---
+++ Solys/Assets/Scripts/Wheel/WheelLogic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelLogic : MonoBehaviour {
+
+	public Vector2 gravityForce;
+	private Vector2 startPosition;
+	private Rigidbody rb;
+
+	/// <summary>
+	/// This function is called when the object becomes enabled and active.
+	/// </summary>
+	void OnEnable()
+	{
+		rb = GetComponent<Rigidbody>();
+		startPosition = transform.position;
+		rb.useGravity = false;
+		rb.Sleep();
+	}
+
+	public void StartSimulation()
+	{
+		rb.WakeUp();
+		SetGravitationVector(gravityForce);
+	}
+
+	public void ResetSimulation()
+	{
+		rb.Sleep();
+		transform.position = startPosition;
+	}
+
+	public void ResetSimulationInNewPosition()
+	{
+		rb.Sleep();
+		startPosition = transform.position;
+	}
+	public void SetGravitationVector(Vector2 force)
+	{
+		if(gravityForce!=null)
+			rb.AddForce(-gravityForce);
+		gravityForce = force;
+		rb.AddForce(gravityForce);
+
+	}
+
+
+}
