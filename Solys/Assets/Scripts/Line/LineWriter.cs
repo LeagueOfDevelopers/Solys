3a45a7ce12c99fd2268b28bb188d7d4673e16d44
Nivel <nivel54@mail.ru>
2017-02-01 18:22:47 +0300

    Рефакторинг
    Сложное название хД Добавил комментарии и переназвал переменные.

--- Solys/Assets/Scripts/Line/LineWriter.cs
+++ Solys/Assets/Scripts/Line/LineWriter.cs
@@ -4,20 +4,20 @@ using Lean.Touch;
 
 public class LineWriter : MonoBehaviour
 {
-    List<GameObject> ListLR;
-    List<Vector3> pos;
-    List<Vector2> posCol;
-    public GameObject LineRenderer;
-    public float DistanceBetweenDots;
-    public float FrequencyPoints;
+    private List<GameObject> ListLineRenderers; //It keeps all LineRenderers
+    private List<Vector3> Positions; //It keeps all finger's positions, until u hold your finger on the screen.
+    private List<Vector2> CollidersPositions; //It keeps all collider's positions, until u hold your finger on the screen.
+    public GameObject LineRenderer; //Our line. It will create, when you touch the screen.
+    public float DistanceBetweenDots; //Input number. 
+    public float FrequencyPoints; //Input number.
     /// <summary>
     /// This function is called when the object becomes enabled and active.
     /// </summary>
     void OnEnable()
     {
-        pos = new List<Vector3>();
-        posCol = new List<Vector2>();
-        ListLR = new List<GameObject>();
+        Positions = new List<Vector3>();
+        CollidersPositions = new List<Vector2>();
+        ListLineRenderers = new List<GameObject>();
         LeanTouch.OnFingerSet += OnFingerSet;
         LeanTouch.OnFingerDown += OnFingerDown;
         LeanTouch.OnFingerUp += OnFingerUp;
@@ -40,123 +40,124 @@ public class LineWriter : MonoBehaviour
     }
     public void OnFingerSet(LeanFinger finger)
     {
-        if (pos.Count == 0)
+        if (Positions.Count == 0) // Когда только что поставили палец на экран
         {
-            pos.Add(finger.GetWorldPosition(10, Camera.current));
-            posCol.Clear();
-            posCol.Add(finger.GetWorldPosition(10, Camera.current));
-            posCol.Add(finger.GetWorldPosition(10, Camera.current));
-            ListLR[ListLR.Count - 1].GetComponent<LineRenderer>().numPositions = pos.ToArray().Length;
-            ListLR[ListLR.Count - 1].GetComponent<LineRenderer>().SetPositions(pos.ToArray());
-            ListLR[ListLR.Count - 1].GetComponent<EdgeCollider2D>().points = posCol.ToArray();
+            Positions.Add(finger.GetWorldPosition(10, Camera.current)); //Самая первая точка касания
+            CollidersPositions.Clear();
+            CollidersPositions.Add(finger.GetWorldPosition(10, Camera.current)); // для коллайдера надо минимум две точки, поэтому создаем две!
+            CollidersPositions.Add(finger.GetWorldPosition(10, Camera.current));
+            ListLineRenderers[ListLineRenderers.Count - 1].GetComponent<LineRenderer>().numPositions = Positions.ToArray().Length;
+            ListLineRenderers[ListLineRenderers.Count - 1].GetComponent<LineRenderer>().SetPositions(Positions.ToArray());
+            ListLineRenderers[ListLineRenderers.Count - 1].GetComponent<EdgeCollider2D>().points = CollidersPositions.ToArray();
         }
-        else
+        else //Движение пальца на экране
         {
-            if (Vector2.Distance(finger.GetWorldPosition(10, Camera.current), posCol[posCol.Count - 1]) > DistanceBetweenDots)
+            if (Vector2.Distance(finger.GetWorldPosition(10, Camera.current), CollidersPositions[CollidersPositions.Count - 1]) > DistanceBetweenDots) //Проверка на минимальное расстояние между точками
             {
-                pos.Add(finger.GetWorldPosition(10, Camera.current));
+                Positions.Add(finger.GetWorldPosition(10, Camera.current)); // Добавляем точку касания.
 
-                List<Vector3> drawingDots = new List<Vector3>();
+                List<Vector3> dotsForDrawing = new List<Vector3>(); //Это лист для хранения точек, которые мы будем рисовать на экране.
 
-                for (int i = 3; i < (pos.Count / 4) * 4; i += 3)
+                for (int i = 3; i < (Positions.Count / 4) * 4; i += 3) // Для каждых 4 точек мы используем формулу безье для нахождения дополнительных точек. (Positions.Count / 4) * 4 используется для того, чтобы убрать остаток. остаток прорабатывается в конце.
                 {
-                    List<Vector3> drawingDotsInFourDots = GetDrawingDots(pos[i - 3], pos[i - 2], pos[i - 1], pos[i]);
+                    List<Vector3> drawingDotsInFourDots = GetAdditionalPoints(Positions[i - 3], Positions[i - 2], Positions[i - 1], Positions[i]); // Данный лист хранит 4 точки, а также дополнительные между ними.
                     for (int ii = 0; ii < drawingDotsInFourDots.Count - 1; ii++)
-                        drawingDots.Add(drawingDotsInFourDots[ii]);
+                        dotsForDrawing.Add(drawingDotsInFourDots[ii]); // Из данного листа мы переносим точки в лист точек для отрисовки.
                 }
 
-                if (pos.Count % 4 != 0)
+                if (Positions.Count % 4 != 0) //Прорабатываем остаток.(если точек 6, то выше мы использовали формулу только для первых четырех точек, оставшиеся две имеют свою фомрулу безье)
                 {
-                    if (pos.Count % 4 == 1) drawingDots.Add(pos[pos.Count - 1]);
+                    if (Positions.Count % 4 == 1) dotsForDrawing.Add(Positions[Positions.Count - 1]); 
 
-                    if (pos.Count % 4 == 2)
+                    if (Positions.Count % 4 == 2)
                     {
-                        List<Vector3> drawingDotsInFourDots = GetDrawingDots(pos[pos.Count - 2], pos[pos.Count - 1]);
-                        for (int ii = 0; ii < drawingDotsInFourDots.Count - 1; ii++)
-                            drawingDots.Add(drawingDotsInFourDots[ii]);
+                        List<Vector3> drawingDotsInTwoDots = GetAdditionalPoints(Positions[Positions.Count - 2], Positions[Positions.Count - 1]);
+                        for (int ii = 0; ii < drawingDotsInTwoDots.Count - 1; ii++)
+                            dotsForDrawing.Add(drawingDotsInTwoDots[ii]);
                     }
 
-                    if (pos.Count % 4 == 3)
+                    if (Positions.Count % 4 == 3)
                     {
-                        List<Vector3> drawingDotsInFourDots = GetDrawingDots(pos[pos.Count - 3], pos[pos.Count - 2], pos[pos.Count - 1]);
-                        for (int ii = 0; ii < drawingDotsInFourDots.Count - 1; ii++)
-                            drawingDots.Add(drawingDotsInFourDots[ii]);
+                        List<Vector3> drawingDotsInThreeDots = GetAdditionalPoints(Positions[Positions.Count - 3], Positions[Positions.Count - 2], Positions[Positions.Count - 1]);
+                        for (int ii = 0; ii < drawingDotsInThreeDots.Count - 1; ii++)
+                            dotsForDrawing.Add(drawingDotsInThreeDots[ii]);
                     }
-                };
-                posCol.Clear();
-                for(int i =0;i<drawingDots.Count;i++)
-                    posCol.Add(drawingDots[i]);
+                }
                 
-                ListLR[ListLR.Count - 1].GetComponent<LineRenderer>().numPositions = drawingDots.ToArray().Length;
-                ListLR[ListLR.Count - 1].GetComponent<LineRenderer>().SetPositions(drawingDots.ToArray());
-                ListLR[ListLR.Count - 1].GetComponent<EdgeCollider2D>().points = posCol.ToArray();
+                CollidersPositions.Clear(); //Сброс листа точек для коллайдера
+                for(int i =0;i< dotsForDrawing.Count;i++) // Все точки для отрисовки мы добавляем в лист точек коллайдера
+                    CollidersPositions.Add(dotsForDrawing[i]);
+
+                ListLineRenderers[ListLineRenderers.Count - 1].GetComponent<LineRenderer>().numPositions = dotsForDrawing.ToArray().Length;
+                ListLineRenderers[ListLineRenderers.Count - 1].GetComponent<LineRenderer>().SetPositions(dotsForDrawing.ToArray());
+                ListLineRenderers[ListLineRenderers.Count - 1].GetComponent<EdgeCollider2D>().points = CollidersPositions.ToArray();
             }
         }
     }
     public void OnFingerDown(LeanFinger finger)
     {
-        ListLR.Add(GameObject.Instantiate(LineRenderer));
+        ListLineRenderers.Add(GameObject.Instantiate(LineRenderer)); //при каждом касании создавать новую линию.
 
     }
 
     public void OnFingerUp(LeanFinger finger)
     {
-        float temporalDistance = DistanceBetweenDots;
-        DistanceBetweenDots = 0;
-        OnFingerSet(finger);
-        DistanceBetweenDots = temporalDistance;
-        pos = new List<Vector3>();
-        posCol = new List<Vector2>();
+        float temporalDistance = DistanceBetweenDots; // Переменная для временного хранения дистанции между точек.
+        DistanceBetweenDots = 0; //Чтобы формула была применима к последней точки касания, мы устанавливаем дистанцию временно равной 0.
+        OnFingerSet(finger); //имитируем касание в точке отрыва пальца
+        DistanceBetweenDots = temporalDistance; // Возвращаем значение дистанции между точек.
+        Positions = new List<Vector3>();
+        CollidersPositions = new List<Vector2>();
 
     }
 
-    private List<Vector3> GetDrawingDots(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    private List<Vector3> GetAdditionalPoints(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3) //Метод для получения дополнительных точек среди 4 точек.
     {
-        List<Vector3> drawingDots = new List<Vector3>();
+        List<Vector3> dotsForDrawing = new List<Vector3>();
         float t = 0.0f;
         while (t < 1.0f)
         {
-            drawingDots.Add(CalculateBezierPoint(t, p0, p1, p2, p3));
-            t += 1.0f / FrequencyPoints;
+            dotsForDrawing.Add(CalculateBezierPoint(t, p0, p1, p2, p3));
+            t += 1.0f / FrequencyPoints; //FrequencyPoints показывает кол-во дополнительных точек.
 
         }
-        drawingDots.Add(CalculateBezierPoint(1.0f, p0, p1, p2, p3));
+        dotsForDrawing.Add(CalculateBezierPoint(1.0f, p0, p1, p2, p3));
 
 
 
-        return drawingDots;
+        return dotsForDrawing;
     }
-    private List<Vector3> GetDrawingDots(Vector3 p0, Vector3 p1, Vector3 p2)
+    private List<Vector3> GetAdditionalPoints(Vector3 p0, Vector3 p1, Vector3 p2)//Метод для получения дополнительных точек среди 3 точек.
     {
-        List<Vector3> drawingDots = new List<Vector3>();
+        List<Vector3> dotsForDrawing = new List<Vector3>();
         float t = 0.0f;
         while (t < 1.0f)
         {
-            drawingDots.Add(CalculateBezierPoint(t, p0, p1, p2));
+            dotsForDrawing.Add(CalculateBezierPoint(t, p0, p1, p2));
             t += 1.0f / FrequencyPoints;
 
         }
-        drawingDots.Add(CalculateBezierPoint(1.0f, p0, p1, p2));
+        dotsForDrawing.Add(CalculateBezierPoint(1.0f, p0, p1, p2));
 
 
-        return drawingDots;
+        return dotsForDrawing;
     }
-    private List<Vector3> GetDrawingDots(Vector3 p0, Vector3 p1)
+    private List<Vector3> GetAdditionalPoints(Vector3 p0, Vector3 p1)//Метод для получения дополнительных точек среди 2 точек.
     {
-        List<Vector3> drawingDots = new List<Vector3>();
+        List<Vector3> dotsForDrawing = new List<Vector3>();
         float t = 0.0f;
         while (t < 1.0f)
         {
-            drawingDots.Add(CalculateBezierPoint(t, p0, p1));
+            dotsForDrawing.Add(CalculateBezierPoint(t, p0, p1));
             t += 1.0f / FrequencyPoints;
 
         }
-        drawingDots.Add(CalculateBezierPoint(1.0f, p0, p1));
+        dotsForDrawing.Add(CalculateBezierPoint(1.0f, p0, p1));
 
 
-        return drawingDots;
+        return dotsForDrawing;
     }
-    private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)//Формула безье для 4 точек.
     {
         float u = 1 - t;
         float tt = t * t;
@@ -172,7 +173,7 @@ public class LineWriter : MonoBehaviour
 
         return p;
     }
-    private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)//Формула безье для 3 точек.
     {
         float u = 1 - t;
         float tt = t * t;
@@ -185,7 +186,7 @@ public class LineWriter : MonoBehaviour
 
         return p;
     }
-    private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1)
+    private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1)//Формула безье для 2 точек.
     {
         float u = 1 - t;
 
