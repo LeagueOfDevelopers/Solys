dbd2c0bd4317b1dd6c9712b929225c170488bf1a
Nivel <nivel54@mail.ru>
2017-02-18 17:10:11 +0300

    Блок ускорения
    Добавил параметром силу с которой будет отбрасывать)
    Текстурку лепил в пэинте)

---
+++ Solys/Assets/Scripts/AccelBlock/Accel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accel : MonoBehaviour
+{
+    public float AccelForce;
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.attachedRigidbody)
+        {
+            Vector3 rotationInEuler = transform.rotation.eulerAngles;
+            var x = Mathf.Cos(rotationInEuler.z*Mathf.PI/180);
+            var y = Mathf.Sin(rotationInEuler.z * Mathf.PI / 180);
+            other.attachedRigidbody.AddForce(new Vector3(x,y) *AccelForce);
+        }
+    }
+}
