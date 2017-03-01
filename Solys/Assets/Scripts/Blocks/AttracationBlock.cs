5a203356fe1a820d5758b213daaf5ad07f0a8289
Nivel <nivel54@mail.ru>
2017-03-01 11:53:06 +0300

    Блок притяжения
    Картинка снова из паинта. Радиус триггера настраивается сам.

---
+++ Solys/Assets/Scripts/Blocks/AttracationBlock.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttracationBlock : MonoBehaviour
+{
+
+    public float RadiusForAttraction;
+    private bool inRange;
+    public GameObject wheel;
+    public float AttracionStrength;
+    private float DistanceBetweenPlayerAndBlock;
+    // Use this for initialization
+	void Start ()
+	{
+	    inRange = false;
+	    GetComponent<CircleCollider2D>().radius = RadiusForAttraction;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (inRange)
+	    {
+	        DistanceBetweenPlayerAndBlock = Vector2.Distance(transform.position, wheel.transform.position);
+	        if (DistanceBetweenPlayerAndBlock <= RadiusForAttraction)
+	        {
+                Vector3 Direction = transform.position - wheel.transform.position;
+
+	            Vector3 Attraction = Direction.normalized * AttracionStrength * (DistanceBetweenPlayerAndBlock) / RadiusForAttraction;
+
+                //wheel.transform.Translate(Attraction,Space.World);
+                wheel.GetComponent<WheelLogic>().AddForce(Attraction);
+
+	        }
+	    }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            inRange = true;
+
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            inRange = false;
+
+    }
+}
