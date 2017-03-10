a54f63dd8f4f4e55e7373e8b3ac43f7cf06fa1cf
Николай Акопов <asoman97@gmail.com>
2017-03-10 15:28:04 +0300

    Добавил меню выбора уровня

---
+++ Solys/Assets/Scripts/Menu/SelectLevel/SelectLevelMenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SelectLevelMenuScript : MonoBehaviour {
+
+	public GameObject Content;
+	public GameObject ButtonTemplate;
+	public int FirstSceneId;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		for(int i = FirstSceneId; i<SceneManager.sceneCountInBuildSettings;i++)
+		{
+			GameObject button = Instantiate(ButtonTemplate,Content.transform);
+			Scene scene = SceneManager.GetSceneByBuildIndex(i);
+			button.transform.localScale = new Vector3(1,1,1);
+			button.transform.FindChild("Text").GetComponent<Text>().text = (i-FirstSceneId+1).ToString();
+			button.GetComponent<SelectLevelButton>().id = i;
+		}
+		
+	}
+
+	public void BackButtonClick()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+	
+}
