dcf2208d34280ede97ffbf76ba56e60dc06a40fc
Nivel <nivel54@mail.ru>
2017-03-10 17:29:43 +0300

    Небольшие настройки
    для продолжения создания скрипта

---
+++ Solys/Assets/Scripts/Camera/CameraLogic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLogic : MonoBehaviour
+{
+
+    public Vector2 size;
+    public Vector2 position;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
