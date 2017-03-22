29cdb634381f6c5b1610670a9c13445aa8b19c7e
Nivel <nivel54@mail.ru>
2017-03-22 11:25:45 +0300

    fixes
    Исправил дергание камеры
    1 ресайз=1 жест

--- Solys/Assets/Scripts/Camera/CameraLogic.cs
+++ Solys/Assets/Scripts/Camera/CameraLogic.cs
@@ -7,37 +7,49 @@ using Lean.Touch;
 public class CameraLogic : MonoBehaviour
 {
     private bool isEnabled = false;
+    private bool isCanSize = true;
     public float distanceForReSize;
     private float LastDistance;
     private Vector3 LastPosition;
     public float[] CameraSizes;
     private int state = 1;
-	void Start () {
-	    LeanTouch.OnFingerDown += OnFingerDown;
-	    LeanTouch.OnFingerUp += OnFingerUp;
+    void Start() {
+        LeanTouch.OnFingerDown += OnFingerDown;
+        LeanTouch.OnFingerUp += OnFingerUp;
         LeanTouch.OnFingerSet += OnFingerSet;
-	}
+    }
     public void OnFingerSet(LeanFinger finger)
     {
-       if (isEnabled)
+        if (isEnabled)
         {
             if (LeanTouch.Fingers.Count == 1)
             {
-                transform.position -=(finger.GetWorldPosition(10, Camera.current) - LastPosition);
+                transform.position -= (finger.GetWorldPosition(10, Camera.current) - LastPosition);
                 LastPosition = finger.GetWorldPosition(10, Camera.current);
             }
             if (LeanTouch.Fingers.Count == 2)
             {
-                if (Mathf.Abs((Vector2.Distance(LeanTouch.Fingers[0].ScreenPosition,
-                LeanTouch.Fingers[1].ScreenPosition) - LastDistance))>distanceForReSize)
+                if (isCanSize)
                 {
-                    float difference = Vector2.Distance(LeanTouch.Fingers[0].ScreenPosition,
-                LeanTouch.Fingers[1].ScreenPosition) - LastDistance;
-                    if (difference > 0) CameraDown();
-                    else CameraUp();
+                    if (Mathf.Abs((Vector2.Distance(LeanTouch.Fingers[0].ScreenPosition,
+                    LeanTouch.Fingers[1].ScreenPosition) - LastDistance)) > distanceForReSize)
+                    {
+                        float difference = Vector2.Distance(LeanTouch.Fingers[0].ScreenPosition,
+                    LeanTouch.Fingers[1].ScreenPosition) - LastDistance;
+                        if (difference > 0)
+                        {
+                            isCanSize = false;
+                            CameraDown();
+                        }
+                        else
+                        {
+                            isCanSize = false;
+                            CameraUp();
+                        }
 
-                    LastDistance = Vector2.Distance(LeanTouch.Fingers[0].ScreenPosition,
-                LeanTouch.Fingers[1].ScreenPosition);
+                        LastDistance = Vector2.Distance(LeanTouch.Fingers[0].ScreenPosition,
+                    LeanTouch.Fingers[1].ScreenPosition);
+                    }
                 }
             }
 
@@ -46,11 +58,11 @@ public class CameraLogic : MonoBehaviour
     }
     public void CameraUp()
     {
-         if (state<2)
-            {
-                state++;
-                GetComponent<Camera>().orthographicSize = CameraSizes[state];
-            }
+        if (state < 2)
+        {
+            state++;
+            GetComponent<Camera>().orthographicSize = CameraSizes[state];
+        }
     }
     public void CameraDown()
     {
@@ -58,25 +70,42 @@ public class CameraLogic : MonoBehaviour
         {
             state--;
             GetComponent<Camera>().orthographicSize = CameraSizes[state];
-            
+
         }
     }
     public void OnFingerUp(LeanFinger finger)
     {
-      
-       
+        if (isEnabled)
+        {
+            if (LeanTouch.Fingers.Count == 2)
+            {
+                if (LeanTouch.Fingers[0].Equals(finger))
+                {
+                    LastPosition = LeanTouch.Fingers[1].GetWorldPosition(10, Camera.current);
+                }
+                else
+                {
+                    LastPosition = LeanTouch.Fingers[0].GetWorldPosition(10, Camera.current);
+                }
+                
+            }
+            isCanSize = true;
+        }
     }
 
     public void OnFingerDown(LeanFinger finger)
     {
-        if (LeanTouch.Fingers.Count == 1)
+        if (isEnabled)
         {
-        LastPosition = finger.GetWorldPosition(10,Camera.current);
-        }
-        if (LeanTouch.Fingers.Count == 2)
-        {
-            LastDistance = Vector2.Distance(LeanTouch.Fingers[0].ScreenPosition, 
-                LeanTouch.Fingers[1].ScreenPosition);
+            if (LeanTouch.Fingers.Count == 1)
+            {
+                LastPosition = finger.GetWorldPosition(10, Camera.current);
+            }
+            if (LeanTouch.Fingers.Count == 2)
+            {
+                LastDistance = Vector2.Distance(LeanTouch.Fingers[0].ScreenPosition,
+                    LeanTouch.Fingers[1].ScreenPosition);
+            }
         }
     }
 
