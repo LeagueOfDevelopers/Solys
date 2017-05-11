15c0d71b89f055f798f209fbc9507405681ef1b4
Nivel <nivel54@mail.ru>
2017-05-11 14:59:07 +0300

    Блок телепорта Анимация

--- Solys/Assets/Scripts/Blocks/TeleportBlock.cs
+++ Solys/Assets/Scripts/Blocks/TeleportBlock.cs
@@ -9,6 +9,12 @@ public class TeleportBlock : MonoBehaviour {
     {
         if (other.attachedRigidbody)
         {
+            Vector2 direction = new Vector2(ExitBlock.transform.position.x - transform.position.x, ExitBlock.transform.position.y - transform.position.y);
+            var velocitySetting = GetComponent<ParticleSystem>().velocityOverLifetime;
+            velocitySetting.x = direction.x/3;
+            velocitySetting.y = direction.y/3;
+            var triggerSetting = GetComponent<ParticleSystem>().trigger;
+            triggerSetting.SetCollider(0, ExitBlock.GetComponent<Collider2D>());
             GetComponent<ParticleSystem>().Play();
             ExitBlock.GetComponent<ParticleSystem>().Play();
             other.transform.position = ExitBlock.transform.position;
