e6fb4983b69a5a5744efa75adc70c9f7dd6637f0
Unknown <nivel54@mail.ru>
2017-08-18 15:59:12 +0300

    Helper
    Добавил префаб Helper-а. 
    После добавления, надо указать сами подсказки в массиве подсказок.

---
+++ Solys/Assets/Scripts/Helper/Helper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Helper : MonoBehaviour {
+
+    private Text tip;
+    private GameObject TextForCloud;
+    private int currentTip;
+    public string[] tips;
+    private GameObject Cloud;
+	// Use this for initialization
+	void Start ()
+    {
+        Cloud = transform.GetChild(0).gameObject;
+        tip = transform.GetChild(1).gameObject.GetComponent<Text>();
+        Button bt = tip.gameObject.AddComponent<Button>();
+        bt.targetGraphic = Cloud.GetComponent<Image>();
+        bt.onClick.AddListener(ShowTip);
+        if (tips == null)
+        {
+            Cloud.SetActive(false);
+            SetText(" ");
+            return;
+        }
+        currentTip = 0;
+        ShowTip();
+    }
+    
+
+    public void ShowTip()
+    {
+        if (currentTip >= tips.Length)
+        {
+            Cloud.SetActive(false);
+            SetText(" ");
+            Destroy(this.gameObject);
+            return;
+        }
+        SetText(tips[currentTip]);
+        int kp = tips[currentTip].Length / 40;
+        kp++;
+        tip.transform.position = this.transform.position; 
+        RectTransform rt = Cloud.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(tip.preferredWidth+30, tip.preferredHeight+30);
+        rt = tip.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2((tip.preferredWidth + 30 )/ kp, tip.preferredHeight );
+
+        currentTip++;
+    }
+
+    public void SetText(string textString)
+    {
+        tip.text = textString;
+
+        Font ArialFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        tip.font = ArialFont;
+        tip.material = ArialFont.material;
+    }
+    
+}
