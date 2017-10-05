9539004b7763eb7d2c59a668719bcaeb2788e235
NickAk <asoman97@gmail.com>
2017-10-05 23:04:09 +0300

    Первый экран

--- Solys/Assets/Scripts/Menu/MainMenuScript.cs
+++ Solys/Assets/Scripts/Menu/MainMenuScript.cs
@@ -1,13 +1,54 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour {
 
-	public void OpenSelectLevelMenu()
-	{
-		SceneManager.LoadScene("SelectPackMenu");
+    public GameObject Logo;
+    public GameObject ButtonText;
+    public GameObject Ball;
 
-	}
+    private Coroutine Wait;
+
+    public void StartLogoAnimEnded()
+    {
+         Wait = StartCoroutine(WaitToContinueAnimation());
+    }
+
+    public void OpenSelectPackMenu()
+    {
+        SceneManager.LoadScene("SelectPackMenu");
+
+    }
+
+    private void Start()
+    {
+        ButtonText.transform.parent.gameObject.GetComponent<Button>().interactable = false;
+    }
+
+    private void Update()
+    {
+        if(Wait!=null && (Input.touchCount>0 || Input.GetMouseButtonDown(0)))
+        {
+            StopCoroutine(Wait);
+            ContinueAnimation();
+        }
+    }
+    
+    private IEnumerator WaitToContinueAnimation()
+    {
+        yield return new WaitForSeconds(5);
+        ContinueAnimation();
+
+    }
+
+    private void ContinueAnimation()
+    {
+        GetComponent<Animator>().SetBool("EndLogo", true);
+        Logo.GetComponent<Animator>().SetBool("EndLogo", true);
+        ButtonText.GetComponent<Animator>().SetBool("EndLogo", true);
+        Ball.GetComponent<Animator>().SetBool("EndLogo", true);
+        ButtonText.transform.parent.gameObject.GetComponent<Button>().interactable = true;
+    }
 }
