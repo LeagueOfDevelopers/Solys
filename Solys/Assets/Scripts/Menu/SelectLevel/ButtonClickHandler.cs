9ebf9b15db1f6ae14d775fac43aaff6fdc2a8879
NickAk <asoman97@gmail.com>
2017-10-11 14:51:08 +0300

    Фикс скрола в меню выбора пака
    Не забыть вернуть нормальные значения для сцен!

---
+++ Solys/Assets/Scripts/Menu/SelectLevel/ButtonClickHandler.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonClickHandler : MonoBehaviour, IPointerUpHandler
+{
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        
+    }
+}
