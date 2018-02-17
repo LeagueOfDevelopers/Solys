7e8c6cf070bad0881665ef64b61190197d9f9a03
NickAk <asoman97@gmail.com>
2018-02-17 21:16:27 +0300

    Some fixes to buy panel

--- Solys/Assets/Scripts/Menu/PowerLabel.cs
+++ Solys/Assets/Scripts/Menu/PowerLabel.cs
@@ -13,6 +13,7 @@ public class PowerLabel : MonoBehaviour {
 	}
     private void OnEnable()
     {
+        SetBuyPanelVisibility(false);
         GeneralLogic.StartSimulationEvent += StartSimulation;
         GeneralLogic.ResetSimulationEvent += ResetSimulation;
         if (BuyPanel && IsNeedToShowBuyPanel())
@@ -29,15 +30,24 @@ public class PowerLabel : MonoBehaviour {
     private bool IsNeedToShowBuyPanel()
     {
         int power = PrefsDriver.GetPower();
-        bool buyShowed = SceneDataTransfer.Instance.BuyShowed;
-        return (!buyShowed && power < 10) || power == 0;
+        //bool buyShowed = SceneDataTransfer.Instance.BuyShowed;
+        return (power < 10);
     }
 
     private void ShowBuyPanel()
     {
+        SetBuyPanelVisibility(true);
         SceneDataTransfer.Instance.BuyShowed = true;
         BuyPanel.GetComponent<Animator>().SetTrigger("Show");
     }
+
+    private void SetBuyPanelVisibility(bool mode)
+    {
+        Image[] renderers = BuyPanel.GetComponentsInChildren<Image>();
+        foreach (Image r in renderers)
+            r.enabled = mode;
+    }
+
 
     public void StartSimulation()
     {
