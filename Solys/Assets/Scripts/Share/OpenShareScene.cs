81586c2ab0e28750d75d5e039dfac6c1b9ebb328
NickAk <asoman97@gmail.com>
2018-02-21 00:49:28 +0300

    Share Screen

---
+++ Solys/Assets/Scripts/Share/OpenShareScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class OpenShareScene : MonoBehaviour {
+
+	public void OnClick()
+    {
+        SceneManager.LoadScene("Share");
+    }
+}
